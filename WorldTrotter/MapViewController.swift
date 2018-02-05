0f72371baf98bf6550355e68b33183cf5dc82226
Justin Weiss <justinweiss@Justins-MacBook-Pro.local>
2018-02-05 09:52:49 -0500

    Chapter 6 complete. Without chapter 5 and chapter 6 challenges.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -13,6 +13,20 @@ class MapViewController: UIViewController {
     
     var mapView: MKMapView!
     
+    //Allows user to change the map type
+    @objc func mapTypeChanged (_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
     override func loadView() {
         //Create a map view
         mapView = MKMapView()
@@ -22,14 +36,28 @@ class MapViewController: UIViewController {
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        //Allow user to change the map type
+        segmentedControl.addTarget(self,
+            action: #selector(MapViewController.mapTypeChanged(_:)),
+            for: .valueChanged)
+        
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
         
-        //let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
-        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
-        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        //Set up top margin/layout guides
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 8)
         
+        //let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        //let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        //Set up side margins/layout guides
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        //Activates margins
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
